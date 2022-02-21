051d5dd8b60ebb2dcbcfd7d89eff2792b029b9aa
Rodrigo Hailer <rodrigohailer@protonmail.com>
2022-02-21 15:23:17 -0300

    bitebank

---
+++ index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+let cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 88833322209;
+const cliente2Agencia = 1001;
+let cliente2Saldo = 0;
+
+console.log(cliente1Nome);
